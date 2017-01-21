7a626a2c924080b55dd82f078e8e2eabf55d7d2e
Tyler <smit2078@msu.edu>
2017-01-21 00:39:57 -0500

    basic Android app with working map

--- app/src/main/java/spartahack2017/ohdeer/MainActivity.java
+++ app/src/main/java/spartahack2017/ohdeer/MainActivity.java
@@ -1,13 +1,35 @@
 package spartahack2017.ohdeer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+	static String AVOIDDEER = "AVOID_DEER";
+	static String POPUPS = "POP_UPS";
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+	}
+
+	public void onLaunchMapButtonClick( View view ){
+		Intent mapActivity = new Intent( this, MapsActivity.class );
+		mapActivity.putExtra( AVOIDDEER, getAvoidDeerCheckBox().isChecked() );
+		mapActivity.putExtra( POPUPS, getPopUpsCheckBox().isChecked() );
+		startActivity( mapActivity );
+		this.finish();
+	}
+
+	private CheckBox getAvoidDeerCheckBox(){
+		return ( CheckBox ) findViewById( R.id.avoidDeerCheckBox );
+	}
+
+	private CheckBox getPopUpsCheckBox(){
+		return ( CheckBox ) findViewById( R.id.popUpsCheckBox );
 	}
 }
